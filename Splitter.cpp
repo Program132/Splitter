e0cad0b758534ba2a4feb944f9bdf53f3e7fe36c
Program <67320703+Program132@users.noreply.github.com>
2022-04-12 20:32:48 +0200

    New split functions for "char"

--- Splitter.cpp
+++ Splitter.cpp
@@ -18,6 +18,13 @@ std::vector<std::string> split(std::string arg) {
     return myVec;
 }
 
+std::vector<char> splitChar(std::string arg) {
+    std::istringstream iss(arg);
+    std::vector<char> vec((std::istream_iterator<char>(iss)), std::istream_iterator<char>());;
+
+    return vec;
+}
+
 std::string toLowerCase(std::string str) {
     std::transform(str.begin(), str.end(), str.begin(), std::tolower);
     return str;
@@ -32,6 +39,12 @@ int main()
 {
     std::string myString = "print Hello everyone, how are you?";
     std::vector<std::string> myVec = split(myString);
+    myString = "Program";
+    std::vector<char> myVecChar = splitChar(myString);
+
+    for (auto element : myVecChar) {
+        std::cout << element << std::endl;
+    }
 
     if (myVec[0] == "print") {
         for (int i = 1; i < myVec.size(); i++) {
